c94ab1508190b1ccf7c78a1768496bcb072180e3
aakash <aakashkkg786@gmail.com>
2023-06-04 22:02:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginTop: "150px" }}>
+        <button style={{ background: "green", height: "35px", borderRadius: "3px", color: "white", marginRight: "10px", width: "80px", cursor: "pointer" }}>
+          add +
+        </button>
+        <div>
+          <div style={{ height: "30px", width: "80px", background: "lightgrey", textAlign: "center", alignItems: "center", padding: "6px 0px", fontSize: "23px" }}>0</div>
+        </div>
+
+        <div>
+          <button style={{ background: "green", height: "35px", borderRadius: "3px", color: "white", marginLeft: "10px", width: "80px", cursor: "pointer" }}>sub-</button>
+        </div>
+      </div>
+
+    </>
   );
 }
 
